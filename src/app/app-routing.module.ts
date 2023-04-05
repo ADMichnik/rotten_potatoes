e0c4f08e587fd91275bd5690b73e660976849cc0
ADMichnik <amek995@gmail.com>
2023-04-05 10:27:33 +0200

    added angular routning

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { MovieComponent } from './movie/movie.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full'},
+  { path: 'login', component: LoginComponent},
+  { path: 'home', component: HomeComponent},
+  { path: 'movie', component: MovieComponent},
+  { path: '**', component: LoginComponent}
+];
+
+// first path redirects to login component when there is no path,
+// last path redirects to login component when there is something else in path
+// beside adding those I neew to add <router-outlet></router-outlet> to app.component.html
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
