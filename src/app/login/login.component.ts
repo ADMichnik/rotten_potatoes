233fcdd4115ca8b6c82beb2a9f9230a0d73f90fa
ADMichnik <amek995@gmail.com>
2023-04-06 13:25:43 +0200

    added error msg if uname, pword are not filled

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -7,4 +7,19 @@ import { Component } from '@angular/core';
 })
 export class LoginComponent {
 
+// using two way binding we can check if required fields are filled
+// if thety are not filled we can display error message next to login button
+
+username = "";
+password = "";
+errorMsg = "";
+
+login() {
+  if (this.username.trim().length === 0) {
+    this.errorMsg = "Username is required."
+  } else if (this.password.trim().length === 0) {
+    this.errorMsg = "Password is required."
+  } 
+}
+
 }
